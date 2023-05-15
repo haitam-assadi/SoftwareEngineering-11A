a00e6bfedf8ccdad711c8ef05fdd869b2e343297
Adelwa <49122676+Adelwa@users.noreply.github.com>
2023-05-15 06:05:33 +0300

    adding create_store button in my_stores page

--- Dev/TradeZone/src/main/java/PresentationLayer/controller/myStoresController.java
+++ Dev/TradeZone/src/main/java/PresentationLayer/controller/myStoresController.java
@@ -3,11 +3,15 @@ package PresentationLayer.controller;
 import CommunicationLayer.Server;
 import DTO.BagDTO;
 import DTO.StoreDTO;
+import PresentationLayer.model.Search;
+import PresentationLayer.model.Store;
 import ServiceLayer.ResponseT;
 import PresentationLayer.model.Alert;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -62,5 +66,37 @@ public class myStoresController {
         list.add(server.getStoreInfo(controller.getName(), "user1_first_store").getValue());
         list.add(server.getStoreInfo(controller.getName(), "user1_second_store").getValue());
         myStores.put("owner", list);
+    }
+
+    @PostMapping("/createStore")
+    public String createStore(@ModelAttribute Store store, Model model){
+        //public ResponseT<StoreDTO> createStore(String memberUserName, String newStoreName){
+        ResponseT<StoreDTO> response = server.createStore(controller.getName(), store.getStoreName());
+        if(response.ErrorOccurred){
+            model.addAttribute("name", controller.getName());
+            model.addAttribute("hasRole", controller.getHasRole());
+            model.addAttribute("logged", controller.getLogged());
+            model.addAttribute("success", alert.isSuccess());
+            model.addAttribute("fail", alert.isFail());
+            model.addAttribute("message", alert.getMessage());
+            model.addAttribute("isEmpty", myStores.isEmpty());
+            model.addAttribute("myStores", myStores);
+//        model.addAttribute("totalPrice", getTotalPrice(bags));
+            alert = new Alert();
+            return "role_page";
+        }
+        ResponseT<Map<String,List<StoreDTO>>> response1  = server.myStores(controller.getName());
+        myStores = response1.getValue();
+        model.addAttribute("name", controller.getName());
+        model.addAttribute("hasRole", controller.getHasRole());
+        model.addAttribute("logged", controller.getLogged());
+        model.addAttribute("success", alert.isSuccess());
+        model.addAttribute("fail", alert.isFail());
+        model.addAttribute("message", alert.getMessage());
+        model.addAttribute("isEmpty", myStores.isEmpty());
+        model.addAttribute("myStores", myStores);
+//        model.addAttribute("totalPrice", getTotalPrice(bags));
+        alert = new Alert();
+        return "role_page";
     }
 }
