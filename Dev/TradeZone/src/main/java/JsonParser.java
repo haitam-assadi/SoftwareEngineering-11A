fd9f65b12f6c71025cdbde74537d3c11a6888c7c
Adelwa <adelwattad32@gmail.com>
2023-06-18 12:17:57 -0700

    adding init files

--- Dev/TradeZone/src/main/java/JsonParser.java
+++ Dev/TradeZone/src/main/java/JsonParser.java
@@ -1,11 +1,15 @@
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.Iterator;
+import java.util.List;
 
 public class JsonParser {
     public static String getJSONFromFile(String filename) {
@@ -57,15 +61,39 @@ public class JsonParser {
             // Parse the JSON string
             JsonNode jsonNode = objectMapper.readTree(strJson);
 
+
+            Iterator names = jsonNode.fieldNames();
+            System.out.println("Names: ");
+            while(names.hasNext()){
+                System.out.println(names.next());
+            }
+
+
             // Retrieve the data
             String data = jsonNode.get("dataBaseUrl").asText();
             int port = jsonNode.get("port").asInt();
-            String credit = jsonNode.get("creditCardUrl").asText();
+
+
+            ArrayNode arrayNode = (ArrayNode) jsonNode.get("login");
+            System.out.println("arrayNode: " + arrayNode);
+            for(JsonNode node: arrayNode){
+                System.out.println("node: " + node);
+                if(node.isArray()){
+                    for(JsonNode elem : node){
+                        System.out.println(elem.asText());
+                    }
+                }
+            }
+            if(arrayNode.isArray()) {
+                for(JsonNode node : arrayNode) {
+                    String elem = node.asText();
+                    System.out.println(elem);
+                }
+            }
 
             // Print the retrieved data
             System.out.println("dataBaseUrl: " + data);
             System.out.println("port: " + port);
-            System.out.println("creditCardUrl: " + credit);
         } catch (IOException e) {
             e.printStackTrace();
         }
